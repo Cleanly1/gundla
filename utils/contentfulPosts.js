9ac260cd4de69ccc2c601a98431a5ed2650b87dc
Mark Dela Cruz <mark@Marks-MBP.lan>
2020-09-02 11:03:12 +0200

    updated

--- utils/contentfulPosts.js
+++ utils/contentfulPosts.js
@@ -1,17 +1,14 @@
-const space = process.env.CONTENTFUL_SPACE_ID;
-const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN;
-
 const client = require("contentful").createClient({
-  space: space,
-  accessToken: accessToken,
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
 });
 
 export async function fetchEntries(content_type) {
   const entries = await client.getEntries({
     content_type: content_type,
   });
-  if (entries.items) return entries.items;
-  console.log(`Error getting Entries for ${contentType.name}.`);
+
+  return entries.items;
 }
 
 export default { fetchEntries };
